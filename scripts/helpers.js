0f3eb9f01526c9019450a7c4e6fdfa7d9b622792
CR0B0T <caio4arp10@gmail.com>
2025-05-23 19:43:16 -0300

    Fixing bugs

---
+++ scripts/helpers.js
@@ -0,0 +1,5 @@
+Hooks.once("init", () => {
+  Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+    return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+  });
+});
