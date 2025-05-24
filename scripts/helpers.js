a3c007687fd66234775405d92ff46a9c8a7a2196
CR0B0T <caio4arp10@gmail.com>
2025-05-24 13:10:18 -0300

    Adding getDirectionFields function

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -2,4 +2,17 @@ Hooks.once("init", () => {
   Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
     return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
   });
+
+  Handlebars.registerHelper("getDirectionFields", function(directionMode) {
+    switch (directionMode) {
+      case "single":
+        return ["All"];
+      case "xy":
+        return ["X", "Y"];
+      case "nesw":
+        return ["N", "E", "S", "W"];
+      default:
+        return [];
+    }
+  });
 });
